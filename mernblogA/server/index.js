20debf648cf3e14e09556ca677266f1fab2b6591
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-07-17 23:10:02 -0600

    change

--- mernblogA/server/index.js
+++ mernblogA/server/index.js
@@ -12,11 +12,7 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:3000', 'https://asilomern.netlify.app','https://mernblog-25mf.onrender.com']
-  }));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
