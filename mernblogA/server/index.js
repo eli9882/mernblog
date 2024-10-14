8dea35f6f6ab9d099a41006e8fba5de442bb0030
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-10-13 20:54:50 -0600

    cors

--- mernblogA/server/index.js
+++ mernblogA/server/index.js
@@ -12,7 +12,11 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
+//app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
+app.use(cors({
+  origin: '*', 
+  credentials: true,
+}));
 app.use(upload({
   useTempFiles: true,  // Habilitar archivos temporales
   tempFileDir: '/tmp/',  // Ruta de archivos temporales
