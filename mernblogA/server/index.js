0bacfc0b83accefd967b13663922bfb865da9c74
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-10-15 19:02:02 -0600

    cambio cors

--- mernblogA/server/index.js
+++ mernblogA/server/index.js
@@ -1,4 +1,4 @@
-/*const express = require('express')
+const express = require('express')
 require('dotenv').config()
 const {connect} = require("mongoose")
 const cors = require('cors')
@@ -12,11 +12,11 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-//app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
-app.use(cors({
+app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
+/*app.use(cors({
   origin: '*', 
   credentials: true,
-}));
+}));*/
 app.use(upload({
   useTempFiles: true,  // Habilitar archivos temporales
   tempFileDir: '/tmp/',  // Ruta de archivos temporales
@@ -33,10 +33,10 @@ app.use(errorHandler)
 
 connect(process.env.MONGO_URI)
 .then(app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
-.catch(error => console.log(error))*/
+.catch(error => console.log(error))
 
 
-const express = require('express');
+/*const express = require('express');
 require('dotenv').config();
 const { connect } = require('mongoose');
 const cors = require('cors');
@@ -85,4 +85,4 @@ app.use(errorHandler);
 connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
   .catch(error => console.log(error));
-
+*/
