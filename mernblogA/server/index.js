1e3c7fef392913cf07601659da9d46c35d12174d
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-10-18 20:45:53 -0600

    upda

--- mernblogA/server/index.js
+++ mernblogA/server/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 require('dotenv').config()
 const {connect} = require("mongoose")
 const cors = require('cors')
@@ -12,13 +12,7 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-//app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
-app.use(cors({
-  origin: ['https://centrohatillosansebastiancr.netlify.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Úsalo solo si manejas cookies
-}));
+app.use(cors({credentials: true, origin: ['https://centrohatillosansebastiancr.netlify.app','http://localhost:3000']}))
 
 app.use(upload({
   useTempFiles: true,  // Habilitar archivos temporales
@@ -36,7 +30,7 @@ app.use(errorHandler)
 
 connect(process.env.MONGO_URI)
 .then(app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
-.catch(error => console.log(error))
+.catch(error => console.log(error))*/
 
 
 /*const express = require('express');
@@ -123,3 +117,44 @@ app.use(errorHandler);
 connect(process.env.MONGO_URI)
   .then(app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
   .catch(error => console.log(error));*/
+
+
+
+const express = require('express')
+require('dotenv').config()
+const {connect} = require("mongoose")
+const cors = require('cors')
+const upload = require('express-fileupload')
+
+
+const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/userRoutes')
+const {notFound, errorHandler} = require('./middleware/errorMiddleware')
+
+const app = express();
+app.use(cors({
+  origin: ['https://centrohatillosansebastiancr.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(upload({
+  useTempFiles: true,  // Habilitar archivos temporales
+  tempFileDir: '/tmp/',  // Ruta de archivos temporales
+}));
+
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+connect(process.env.MONGO_URI)
+.then(app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
+.catch(error => console.log(error))
